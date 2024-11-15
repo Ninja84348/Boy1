b6e4bd08a40f241ddae3b8fedd070c7105962efb
Ninja84348 <nn4766189@gmail.com>
2024-11-15 22:51:05 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY KaliaYtOwner
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7967283892:AAESDhs5ajDfzw4LRaB5M7aGeNXiXLeTC8A")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Ninja2399")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6517590211")
